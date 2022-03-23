eab6eeaa4efab9a2cf39cb9cedbfd3b110d5fa36
Siharany Nina Nong <ninanongsoth@Siharanys-MacBook-Pro.local>
2022-03-22 23:12:36 -0400

    Added testing framework

---
+++ tests/unit-tests/dummy.spec.ts
@@ -0,0 +1,11 @@
+import 'jest';
+
+describe('dummy', () => {
+    beforeEach(() => {
+        console.log('Before each');
+    });
+
+    it('should work as expected', async () => {
+        console.log('it should work!');
+    });
+});
