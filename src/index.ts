0e032e8f21ff8c7e91b4c1ed17fc2f789ef1b0d0
William Chen <wchen298@gmail.com>
2022-03-27 21:55:29 -0700

    add webpack process

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import * as Parser from './parser';
+import * as AST from './ast/nodes';
+
+export { Parser, AST };
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,8 @@ const webConfig = {
     fallback: {
       stream: require.resolve('stream-browserify'),
       buffer: require.resolve('buffer'),
+      assert: require.resolve('assert'),
+      'process/browser': require.resolve('process'),
     },
   },
   plugins: [
